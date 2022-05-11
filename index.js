baeb96ce0f1a59b3972dc430af5fd4c317dfec17
shakil0719 <ARIYAKHAN995@GMAIL.COM>
2022-05-12 02:37:17 +0600

    access token

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ async function run() {
     app.post("/login", async (req, res) => {
       const user = req.body;
       const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1d",
+        expiresIn: "2d",
       });
       res.send({ accessToken });
     });
